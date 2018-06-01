39ae0dc04348cc9415c6b1ee87ff6bdcfa98b97a
Sean Brage <seanmbrage@me.com>
2018-06-01 09:03:33 -0600

    added material library and toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Component Imports
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+// Module Imports
+import { MaterialComponentsModule } from './material-components/material-components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
